584e3c19c31e73b13b90fa9a16b48fd92a5cf3bf
Rafi <rafistep98@gmail.com>
2026-04-14 01:49:00 -0400

    change prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://phimrgfnwwzgrcsnpusc.supabase.co',
   supabaseAnonKey: 'sb_publishable_vJYaLILUvBfLRAe4sucU3w_IuhzruAc',
-  apiUrl: '/api/v1',
+  apiUrl: 'https://hyerd-production.up.railway.app/api/v1',
 };
